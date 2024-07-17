c9d4a46f7dfc8020bbd9ff0a148618eacabb93a8
juiceman1014 <justintran1014@gmail.com>
2024-07-16 23:29:07 -0700

    added three example cars onto the page

--- cars2compare/src/pages/CarsSavedPage.jsx
+++ cars2compare/src/pages/CarsSavedPage.jsx
@@ -1,5 +1,48 @@
+import carImg from "../assets/carImg1.png";
+
 const CarsSavedPage = () => {
-  return <div>CarsSavedPage</div>;
+  return (
+    <>
+      <div className = "h-screen">
+        <h1 className = "text-center mt-[20px] text-xl">Saved Cars</h1>
+        <div className = "flex flex-col">
+          <car className = "border-black m-[10px] border-[3px] flex flex-row p-[10px] justify-between items-center">
+            <img className = "w-[300px] h-auto" src = {carImg}></img>
+            <p>Name:</p>
+            <p>Price:</p>
+            <p>MPG:</p>
+            <p>HP:</p>
+            <p>Engine:</p>
+            <p>Transmission:</p>
+            <p>Weight:</p>
+          </car>
+
+          <car className = "border-black m-[10px] border-[3px] flex flex-row p-[10px] justify-between items-center">
+            <img className = "w-[300px] h-auto" src = {carImg}></img>
+            <p>Name:</p>
+            <p>Price:</p>
+            <p>MPG:</p>
+            <p>HP:</p>
+            <p>Engine:</p>
+            <p>Transmission:</p>
+            <p>Weight:</p>
+          </car>
+
+          <car className = "border-black m-[10px] border-[3px] flex flex-row p-[10px] justify-between items-center">
+            <img className = "w-[300px] h-auto" src = {carImg}></img>
+            <p>Name:</p>
+            <p>Price:</p>
+            <p>MPG:</p>
+            <p>HP:</p>
+            <p>Engine:</p>
+            <p>Transmission:</p>
+            <p>Weight:</p>
+          </car>
+
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CarsSavedPage;
