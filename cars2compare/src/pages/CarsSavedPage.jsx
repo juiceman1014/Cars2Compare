ac9bf116a6257fc4645dff4c8ac89384e8d287c3
juiceman1014 <justintran1014@gmail.com>
2024-07-17 22:34:55 -0700

    adjusted attribute implementation

--- cars2compare/src/pages/CarsSavedPage.jsx
+++ cars2compare/src/pages/CarsSavedPage.jsx
@@ -5,10 +5,10 @@ import { useState } from 'react';
 const CarsSavedPage = () => {
 
   const [cars, setCars] = useState([
-    {id: 1, name: '', price: '', mpg: '', hp: '', engine: '', transmission: '', weight: ''},
-    {id: 2, name: '', price: '', mpg: '', hp: '', engine: '', transmission: '', weight: ''},
-    {id: 3, name: '', price: '', mpg: '', hp: '', engine: '', transmission: '', weight: ''},
-    {id: 4, name: '', price: '', mpg: '', hp: '', engine: '', transmission: '', weight: ''}
+    {id: 1, img: carImg, name: '', price: '', mpg: '', hp: '', engine: '', transmission: '', weight: ''},
+    {id: 2, img: carImg, name: '', price: '', mpg: '', hp: '', engine: '', transmission: '', weight: ''},
+    {id: 3, img: carImg, name: '', price: '', mpg: '', hp: '', engine: '', transmission: '', weight: ''},
+    {id: 4, img: carImg, name: '', price: '', mpg: '', hp: '', engine: '', transmission: '', weight: ''}
   ]);
 
   const handleDelete = (id) => {
@@ -24,15 +24,15 @@ const CarsSavedPage = () => {
           <car key = {car.id} className = "border-black m-[10px] border-[3px] flex flex-row p-[10px] justify-between items-center flex-wrap">
             <div>
               <button className = "text-[red]" onClick = {() => handleDelete(car.id)}>Delete</button>
-              <img className = "w-[300px] h-auto" src = {carImg} alt = "car image"></img>
+              <img className = "w-[300px] h-auto" src = {car.img} alt = "car image"></img>
             </div>
-            <p>Name:</p>
-            <p>Price:</p>
-            <p>MPG:</p>
-            <p>HP:</p>
-            <p>Engine:</p>
-            <p>Transmission:</p>
-            <p>Weight:</p>
+            <p>Name: {car.name}</p>
+            <p>Price: {car.price}</p>
+            <p>MPG: {car.mpg}</p>
+            <p>HP: {car.hp}</p>
+            <p>Engine: {car.engine}</p>
+            <p>Transmission: {car.transmission}</p>
+            <p>Weight: {car.weight}</p>
           </car>
       ))}
         </carcontainer>
