30e1e1eca4a21f485d10fc61c29fb5b564309c33
juiceman1014 <justintran1014@gmail.com>
2024-07-18 17:59:48 -0700

    added route for register page

--- cars2compare/src/App.jsx
+++ cars2compare/src/App.jsx
@@ -3,8 +3,10 @@ import IntroductionPage from "./pages/introductionPage";
 import SearchPage from "./pages/searchPage.jsx";
 import SignInPage from "./pages/signInPage.jsx";
 import CarsSavedPage from "./pages/CarsSavedPage.jsx";
+import RegisterPage from "./pages/registerPage.jsx";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer.jsx";
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         <Route path="/Search" element={<SearchPage />} />
         <Route path="/CarsSaved" element={<CarsSavedPage />} />
         <Route path="/SignIn" element={<SignInPage />} />
+        <Route path="/Register" element = {<RegisterPage/>} /> 
       </Routes>
       <Footer></Footer>
     </>
