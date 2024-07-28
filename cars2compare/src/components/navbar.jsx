be0ff161b144364796fa3a1b28870d50e83bb2cb
juiceman1014 <justintran1014@gmail.com>
2024-07-27 23:04:47 -0700

    navbar changes depending on whether or not user is logged in

--- cars2compare/src/components/navbar.jsx
+++ cars2compare/src/components/navbar.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { UserContext } from "../context/UserContext.jsx";
+
 const Navbar = () => {
+  const { user, logout } = useContext(UserContext);
+
   return (
     <>
       <nav className="flex flex-row items-baseline justify-between px-5 lg:px-20 py-4 font-bold">
@@ -7,16 +12,25 @@ const Navbar = () => {
           Cars2Compare
         </Link>
         <ul className="flex gap-5 items-baseline">
-          <CustomLink to="/Search"> Search</CustomLink>
+          <CustomLink to="/Search">Search</CustomLink>
+        {user ? ( 
+          <>
           <CustomLink to="/CarsSaved">Saved Cars</CustomLink>
           <CustomLink to="/Review">Review A Car</CustomLink>
-
-          <CustomLink
+          <div>Hello, {user.username}</div>
+          <button 
+          onClick = {logout}
+          >
+            Logout
+          </button>
+          </>
+        ) : (  <CustomLink
             to="/SignIn"
             className="border-2 border-black p-2 rounded-xl hover:bg-black hover:text-white"
           >
             Sign In
           </CustomLink>
+        )}
         </ul>
       </nav>
     </>
