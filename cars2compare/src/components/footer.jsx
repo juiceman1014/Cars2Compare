e49c51276690520673d9942c7ab76fb9f71fc4e9
juiceman1014 <justintran1014@gmail.com>
2024-08-04 19:07:50 -0700

    updated footer with team name

--- cars2compare/src/components/footer.jsx
+++ cars2compare/src/components/footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className = "text-center">Team Name © 2024</div>;
+  return <div className = "text-center">Cars2Compare Inc. © 2024</div>;
 };
 
 export default Footer;
